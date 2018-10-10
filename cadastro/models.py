3e604d63ee1b4bb45fe364a017cfd775d2a51024
Matheus <matheusrf96@gmail.com>
2018-10-10 15:15:34 -0300

    Criação da model Pessoa

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField('Nome', max_length=150)
+    cpf = models.CharField('CPF', max_length=15)
+    rg = models.CharField('RG', max_length=15)
+    endereco = models.CharField('Endereço', max_length=255) 
+    bairro = models.CharField('Bairro', max_length=100)
+    cidade = models.CharField('Cidade', max_length=100)
+    estado = models.CharField('Estado', max_length=2)
+
+    def __str__(self):
+        return self.nome
+        
