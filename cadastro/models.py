45b6cbdfdb4081f6517b9e23334be61787c8029b
Matheus <matheusrf96@gmail.com>
2018-10-30 13:02:00 -0300

    Definição da imagem padrão

--- cadastro/models.py
+++ cadastro/models.py
@@ -7,7 +7,7 @@ class Pessoa(models.Model):
     nome = models.CharField('Nome', max_length=150)
     cpf = models.CharField('CPF', max_length=15)
     rg = models.CharField('RG', max_length=15)
-    foto_principal = models.ImageField('Foto do Visitante', upload_to='cadastro/static/img/upload/visitantes/principal/{}'.format(time.strftime("%Y/%m/%d")), blank=True)
+    foto_principal = models.ImageField('Foto do Visitante', upload_to='cadastro/static/img/upload/visitantes/principal/{}'.format(time.strftime("%Y/%m/%d")), blank=True, default='cadastro/static/img/user.svg')
     foto_adicional = models.ImageField('Foto Adicional', upload_to='cadastro/static/img/upload/visitantes/adicional/{}'.format(time.strftime("%Y/%m/%d")), blank=True)
     endereco = models.CharField('Endereço', max_length=255) 
     bairro = models.CharField('Bairro', max_length=100)
